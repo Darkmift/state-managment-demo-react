2c331aa1b11e521bfa4d1a131e0010eb889fb389
darkmift <darkmift@gmail.com>
2023-02-06 23:39:03 +0200

    finalized user slice

--- src/store/slices/user.slice.js
+++ src/store/slices/user.slice.js
@@ -1,17 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import localStorageService from '../../utils/localStorage';
+import loginMockService from '../login.mock-service';
+
+const fetchUser = createAsyncThunk('user/fetchUser', loginMockService);
 
 function setUser(s, { name, email }) {
   s.name = name;
   s.email = email;
+  localStorageService.setData('user', { name, email });
 }
 
 export const userSlice = createSlice({
   name: 'user',
   initialState: { email: '', name: '' },
   reducers: {
-    setFromLocalStorage: () => {
+    isLoggedin: (state) => {
+      return state.name.length;
+    },
+    setFromLocalStorage: (state) => {
       const storedUser = localStorageService.getData('user');
+      if (!storedUser) return;
       const { name, email } = storedUser;
       if (name?.length && email.length) {
         setUser(state, { name, email });
@@ -25,9 +33,16 @@ export const userSlice = createSlice({
       setUser(state, { name: null, email: null });
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(fetchUser.fulfilled, (state, action) => {
+      const { email, name } = action.payload;
+      setUser(state, { name, email });
+    });
+  },
 });
 
 // Action creators are generated for each case reducer function
-export const { setFromLocalStorage, logUser, removeUser } = userSlice.actions;
+export const { setFromLocalStorage, logUser, removeUser,isLoggedin } = userSlice.actions;
+export { fetchUser };
 
 export default userSlice.reducer;

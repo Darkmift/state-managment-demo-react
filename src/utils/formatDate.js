2d77a446aa86a0afd5bed9a6c992d75a3d1ad363
darkmift <darkmift@gmail.com>
2023-02-07 14:21:49 +0200

    added fallback

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,6 +1,14 @@
 export default function formatDate(date) {
-  const day = `0${date.getDate()}`.slice(-2);
-  const month = `0${date.getMonth() + 1}`.slice(-2);
-  const year = date.getFullYear();
-  return `${day}-${month}-${year}`;
+  try {
+    if (typeof date === 'string') {
+      date = new Date(date);
+    }
+    const day = `0${date.getDate()}`.slice(-2);
+    const month = `0${date.getMonth() + 1}`.slice(-2);
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
+  } catch (error) {
+    console.log('🚀 ~ file: formatDate.js:10 ~ formatDate ~ error', { date, error });
+    return 'oops';
+  }
 }

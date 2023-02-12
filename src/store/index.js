19ca43df72f560ff3d5f44811bbcb5666286e230
darkmift <darkmift@gmail.com>
2023-02-12 15:47:58 +0200

    rewrote logout logic

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import usersReducer from './slices/user.slice';
+import usersReducer, { removeUser } from './slices/user.slice';
 import todosReducer from './slices/todos.slice';
 
 import { setFromLocalStorage } from './slices/user.slice';
@@ -15,23 +15,16 @@ const store = configureStore({
     }),
 });
 
-// function kickUserIfNotSet() {
-//   const state = store.getState();
-//   console.log("🚀 ~ file: index.js:19 ~ kickUserIfNotSet ~ state", state)
-
-//   if (state.users.name.length < 1) {
-//     router.navigate('/login');
-//   }
-// }
-
 store.dispatch(setFromLocalStorage(''));
 
-// kickUserIfNotSet();
-// // const unsubscribe =
-// store.subscribe(() => {
-//   kickUserIfNotSet();
-//   //inject to ls
-// });
-// // unsubscribe();
+const unsubscribe = store.subscribe(() => {
+  const state = store.getState();
+  if (state.users.name.length > 0) {
+    console.log('User name is present in the store');
+    router.navigate('/login');
+    store.dispatch(removeUser())
+    unsubscribe();
+  }
+});
 
 export default store;
